6c436422ca180142299114551ae2e4e2dd0cb96f
Pratiksha Nikam <pratiksha.gawande@gmail.com>
2024-11-28 23:58:09 +0530

    api added

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
@@ -8,14 +8,16 @@ import styles from "./Navbar.module.css";
 function Navbar({ searchData }) {
   return (
     <nav className={styles.navbar}>
-    
+    <Link to="/">
         <Logo />
+        </Link>
      
       <Search
         placeholder="Search a song of your choice"
         searchData={searchData}
       />
-      <button className={styles.button}>Give Feedback</button>
+      {/* <button className={styles.button}>Give Feedback</button> */}
+      <Button >Give Feedback</Button>
     </nav>
   );
 }
