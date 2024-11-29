1359dc8f8ff00d24a3e5f118fc33bcd77557eeba
Pratiksha Nikam <pratiksha.gawande@gmail.com>
2024-11-29 18:26:19 +0530

    filter folder name changed

--- qtify/src/components/filters/Filters.jsx
+++ qtify/src/components/filters/Filters.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+//import Typography from "@mui/material/Typography";
+//import Box from "@mui/material/Box";
 import styles from "./Filters.module.css";
 
 // function TabPanel(props) {
